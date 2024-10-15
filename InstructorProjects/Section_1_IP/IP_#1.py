af0e9e7bc5d37849ba9d7acf2ac526c69af6a994
Tahir Hussain <tahir@waveaxis.com>
2024-10-14 21:04:27 -0500

    section 1 instructor project completed

--- InstructorProjects/Section_1_IP/IP_#1.py
+++ InstructorProjects/Section_1_IP/IP_#1.py
@@ -6,4 +6,24 @@ from datetime import datetime
 
 directory = 'files'
 
-filepaths = os.listdir(directory)
+filenames = os.listdir(directory)
+
+for filename in filenames:
+    filepath = os.path.join(directory, filename)
+
+    with open(filepath, 'r') as file:
+        content = file.read()
+        words = content.split()
+        word_count = len(words)
+        print(word_count)
+
+    day = datetime.now().strftime("%a")
+    new_filename = f'{filename[:-4]}-{word_count}-{day}.txt' 
+
+    new_filepath = os.path.join(directory, new_filename)
+
+    os.rename(filepath, new_filepath)
+
+    print("Renamed {filename} to {new_filename}")
+    
+
