dcede165eb8597706596fb7ab8d691912d1e4250
Tahir Hussain <tahir@waveaxis.com>
2024-10-14 19:39:13 -0500

    section 1 first commit

--- InstructorProjects/Section_1_IP/IP_#1.py
+++ InstructorProjects/Section_1_IP/IP_#1.py
@@ -1,2 +1,9 @@
 #Project Description
-    # Hi My name is Tahir Hussain
+    # How to rename files with python, and classifying it by number of words and the day of the week.
+
+import os
+from datetime import datetime
+
+directory = 'files'
+
+filepaths = os.listdir(directory)
