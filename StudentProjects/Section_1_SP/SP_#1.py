7c26f073b7494e202e6f7844bd8f505dc7ea1984
TahirHussain-Repo <121993851+TahirHussain-Repo@users.noreply.github.com>
2024-11-16 15:12:11 -0600

    Update SP_#1.py

--- StudentProjects/Section_1_SP/SP_#1.py
+++ StudentProjects/Section_1_SP/SP_#1.py
@@ -1,16 +1,17 @@
 #Project Description
     # How to rename files with python, and classifying it by number of words and the day of the week.
-
 import os
 from datetime import datetime
 
 directory = 'files'
-
-originalFiles = os.listdir(directory)
-
-for ogFiles in originalFiles:
-    print(ogFiles)
-    
-    filepath = os.path.join(directory, ogFiles)
+filenames = os.listdir(directory)
+for filename in filenames:
+    filepath = os.path.join(directory, filename)
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    new_filename = f'{filename[:-4]}-{current_date}.txt'
+    new_filepath = os.path.join(directory, new_filename)
+    os.rename(filepath, new_filepath)
+    print(f"Renamed {filename} to {new_filename}")
+print("File renaming completed!")
 
      
